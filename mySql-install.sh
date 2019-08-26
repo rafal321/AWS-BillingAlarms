5c456f3d06aecbc9fed87eed4a87163b059b6c5a
Rafal <therafk@hotmail.com>
2019-08-26 21:29:37 +0100

    Update mySql-install.sh

--- mySql-install.sh
+++ mySql-install.sh
@@ -1,3 +1,5 @@
+# need for AmazonLinux2: sudo yum install ncurses-compat-libs
+
 sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
 sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm 
 sudo yum install mysql-community-server
@@ -5,3 +7,30 @@ systemctl start mysqld.service
 
 grep "temporary password" /var/log/mysqld.log	
 sudo mysql -u root -p***
+
+
+
+
+### FROM LA ###############################################################
+Installation:
+
+Download the MySQL RPM bundle tar file from the MySQL Community Server downloads page:
+
+ # wget https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.15-1.el7.x86_64.rpm-bundle.tar
+Extract the RPM files from the MySQL bundle tar file:
+
+ # tar -xvf mysql-8.0.15-1.el7.x86_64.rpm-bundle.tar
+Install the MySQL server and dependencies using the rpm command:
+
+ # sudo rpm -Uvh mysql-community-{server,client,common,libs}-*
+Start up the MySQL server:
+
+ # sudo systemctl start mysqld
+Log into the MySQL server as the root user, using the temporary password located in /var/log/mysqld.log:
+
+ # sudo grep 'temporary password' /var/log/mysqld.log
+ # mysql -u root -p
+Update the password for the root user:
+
+ mysql> ALTER USER 'root'@'localhost' IDENTIFIED BY 'PASSWORD';
+ mysql> exit
