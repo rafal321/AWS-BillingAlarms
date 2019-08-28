f5c9e1344aee68e606675ac89ecf3cf05362d8ac
Rafal <therafk@hotmail.com>
2019-08-28 04:54:07 +0100

    Update mySql-mariadb.sh

--- mySql-mariadb.sh
+++ mySql-mariadb.sh
@@ -7,10 +7,25 @@ sudo systemctl start mariadb.service
 mysql_secure_installation
 ###  LOG FILE
 less /var/log/mariadb/mariadb.log
-### CONFIG
+### MAIN CONFIG FILE - tarik
 less /etc/my.cnf.d/server.cnf
 ### UNIT file - not to be messed with
 # /lib/systemd/system/mariadb.service
 
+
+
 ### My usefull commands
+# ctrl+d   to exit
 ss -tulpn | grep mysql
+
+### backups + passwd file
+
+
+
+### if you want to run  on non standard port
+vim /etc/my.cnf
+ # add port=3360 e.g.
+firewall-cmd --permanent --add-port=3360/tcp
+firewall-cmd --reload
+semanage port -a -t mysqld_port -p tcp 3360
+semanage port -l
