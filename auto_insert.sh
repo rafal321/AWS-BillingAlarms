2ffab61a847e580f7f51d70558f19d4f44f0041f
Rafal <therafk@hotmail.com>
2020-03-29 16:57:49 +0100

    Update auto_insert.sh

--- auto_insert.sh
+++ auto_insert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# script for generating inserts into a database
-# for testing galera cluster, master slave replication etc...
+# script generates inserts into a database
+# good for testing galera cluster, master slave replication etc...
 
 INSERTS_NO=10
 INTERVAL=0.5
@@ -18,10 +18,12 @@ for i in $(seq 1 ${INSERTS_NO});do
 #mysql -e "insert into autodb.autotbl1 (description,${SERVER_IP}sequence) values('${MESSAGE}','${x}');"
 mysql -u auto_user -pauto_paSs987* -h ${SERVER_IP} -e "insert into autodb.autotbl1 (description,sequence) values('${MESSAGE}','${x}');" >/dev/null 2>&1
 sleep ${INTERVAL}
+printf '\r%8d %8s' ${x} "rows inserted"
 done
 
 #mysql -e "insert into autodb.autotbl1 (description) values('-----------');"
 mysql -u auto_user -pauto_paSs987* -h ${SERVER_IP} -e "insert into autodb.autotbl1 (description) values('-----------');" >/dev/null 2>&1
+echo
 echo
 printf " --- Finished ---${C_NC}\n"
 echo
@@ -42,11 +44,11 @@ echo
 
 #====Sample output============================
 mysql> select * from autodb.autotbl1;
-+-----+-----------------+----------+---------------------+
-| id  | description     | sequence | TimeStamp           |
-+-----+-----------------+----------+---------------------+
-|   1 | message-1       |        1 | 2020-03-29 13:29:13 |
-|   2 | message-1       |        2 | 2020-03-29 13:29:21 |
-|   3 | message-1       |        3 | 2020-03-29 13:29:29 |
-|   4 | message-1       |        4 | 2020-03-29 13:29:37 |
-|   5 | message-1       |        5 | 2020-03-29 13:29:46 |
++-----+---------------+----------+---------------------+
+| id  | description   | sequence | TimeStamp           |
++-----+---------------+----------+---------------------+
+|   1 | your-message  |        1 | 2020-03-29 13:29:13 |
+|   2 | your-message  |        2 | 2020-03-29 13:29:21 |
+|   3 | your-message  |        3 | 2020-03-29 13:29:29 |
+|   4 | your-message  |        4 | 2020-03-29 13:29:37 |
+|   5 | your-message  |        5 | 2020-03-29 13:29:46 |
