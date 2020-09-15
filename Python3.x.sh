7653074f24ff252feade9dad9db8e0c6a120eeca
Rafal <therafk@hotmail.com>
2020-09-15 19:14:45 +0100

    Update Python3.x.sh

--- Python3.x.sh
+++ Python3.x.sh
@@ -1,5 +1,7 @@
 
 #______________________________________________________
+# https://stackoverflow.com/questions/45542690/how-to-set-python3-5-2-as-default-python-version-on-centos/48757130#48757130
+# https://web.archive.org/web/20200523111449/https://www.redhat.com/sysadmin/alternatives-command
 # Start by registering python2 as an alternative
 alternatives --install /usr/bin/python python /usr/bin/python2 50
 
